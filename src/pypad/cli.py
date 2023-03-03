b0c2b8961e56b7686fb3e07226273bd20dddb144
Alex Cross <alexjcross90@gmail.com>
2023-03-03 22:18:30 +0000

    Kaggle notebook

--- src/pypad/cli.py
+++ src/pypad/cli.py
@@ -3,7 +3,9 @@ from .mnist_mlp import mlp_run
 from .mnist_svm import svm_baseline
 from .mnist_torch import run_torch
 
-from .connectx import Board, Solver
+from .connectx import Board
+from .solver import Solver
+
 from .kaggle_sandbox import run_kaggle
 
 import numpy as np
