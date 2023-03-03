67a08defb7246bf9a46a459c9c804e3d477977d6
Alex Cross <alexjcross90@gmail.com>
2023-03-03 22:18:24 +0000

    Improved heuristics

--- src/pypad/kaggle_sandbox.py
+++ src/pypad/kaggle_sandbox.py
@@ -1,31 +1,26 @@
 import numpy as np
-import random
 from functools import partial
 
 from kaggle_environments import make
 
 from .connectx import Board
-from .solver import Solver
 
 
 
 # Helper function for score_move: calculates value of heuristic for grid
-def get_heuristic(grid, mark, config):
-    num_threes = count_windows(grid, 3, mark, config)
-    num_fours = count_windows(grid, 4, mark, config)
-    num_threes_opp = count_windows(grid, 3, mark%2+1, config)
-    score = num_threes - 1e2*num_threes_opp + 1e6*num_fours
-    return score
-
-def get_heuristic2(board) -> int:
+def get_heuristic(board) -> int:
     directions = (board.mask_utils.rows - 1, board.mask_utils.rows, board.mask_utils.rows + 1, 1)
-    bitboard = board.position ^ board.mask;
-    count = 0
+    bitboard = board.position ^ board.mask
+    bitboard2 = board.position
+    score = 0
     for dir in directions:
-        if bitboard & (bitboard >> dir) & (bitboard >> 2 * dir):
-            count += 0.1
-
-    return count
+        if result := bitboard & (bitboard >> dir) & (bitboard >> 2 * dir):
+            score += 0.1 * result.bit_count()
+            
+        if result := bitboard2 & (bitboard2 >> dir) & (bitboard2 >> 2 * dir):
+            score -= 0.2 * result.bit_count()
+            
+    return score
 
 
 def shallow_negamax(board: Board, alpha: int, beta: int, depth: int) -> int:
@@ -42,7 +37,7 @@ def shallow_negamax(board: Board, alpha: int, beta: int, depth: int) -> int:
         return max_possible_score
     
     if depth == 0:
-        return get_heuristic2(board)
+        return get_heuristic(board)
 
     alpha = -100_000_000
     beta = min(beta, max_possible_score)
@@ -57,16 +52,23 @@ def shallow_negamax(board: Board, alpha: int, beta: int, depth: int) -> int:
 
     return alpha
 
-def agent_negamax(obs, config):
+def agent_negamax(obs, config, depth):
 
     grid = np.asarray(obs.board).reshape(config.rows, config.columns)
     board = Board.from_grid(grid)
 
     best_col, best_score = next(board.possible_col_moves()), -1_000_000
+
     for col in board.possible_col_moves():
         b = board.copy()
         b.play_col(col)
-        alpha, beta, depth = -1, 1, 6
+        if b.is_won():
+            return col
+
+    for col in board.possible_col_moves():
+        b = board.copy()
+        b.play_col(col)
+        alpha, beta = -1, 1
         score = -shallow_negamax(b, alpha, beta, depth)
         if score > best_score:
             best_score = score
@@ -74,25 +76,11 @@ def agent_negamax(obs, config):
             
     return best_col
 
-def agent_negamax2(obs, config):
-
-    grid = np.asarray(obs.board).reshape(config.rows, config.columns)
-    board = Board.from_grid(grid)
-
-    best_col, best_score = next(board.possible_col_moves()), -1_000_000
-    for col in board.possible_col_moves():
-        b = board.copy()
-        b.play_col(col)
-        alpha, beta, depth = -1, 1, 4
-        score = -shallow_negamax(b, alpha, beta, depth)
-        if score >= best_score:
-            best_score = score
-            best_col = col
-            
-    return best_col
-
 def run_kaggle() -> None:
+    agent_negamax5 = partial(agent_negamax, depth=5)
+    agent_negamax2 = partial(agent_negamax, depth=2)
+
     # Setup a tictactoe environment.
     env = make("connectx", debug=True)
-    env.run([agent_negamax2, agent_negamax])
+    env.run([agent_negamax2, agent_negamax5])
     env.render(mode="ipython")
