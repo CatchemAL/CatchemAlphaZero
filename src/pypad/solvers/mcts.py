d4dfc846d5bb7c1eecf9b279c305e44421a7ab69
Alex Cross <alexjcross90@gmail.com>
2023-05-25 23:07:59 +0100

    Sidestep slight issue around state cloning

--- src/pypad/solvers/mcts.py
+++ src/pypad/solvers/mcts.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from math import log, sqrt
 from typing import Generic, List
 
-from ..states import State, TMove, TState
+from ..states import State, Status, TMove, TState
 from . import Solver
 
 
@@ -14,17 +14,17 @@ class Node(Generic[TMove]):
     __slots__ = ["move", "parent", "played_by", "wins", "visit_count", "children", "unexplored_moves"]
 
     def __init__(
-        self, state: State[TMove], parent: Node[TMove] | None = None, move: TMove | None = None
+        self, status: Status[TMove], parent: Node[TMove] | None = None, move: TMove | None = None
     ):
         self.move = move
         self.parent = parent
-        self.played_by = state.played_by
+        self.played_by = status.played_by
 
         self.wins: int = 0
         self.visit_count: int = 0
 
         self.children: List[Node[TMove]] = []
-        self.unexplored_moves: List[TMove] = state.status().legal_moves
+        self.unexplored_moves: List[TMove] = status.legal_moves
 
     @property
     def has_legal_moves(self) -> bool:
@@ -72,7 +72,7 @@ class MctsSolver(Solver):
         return max_child.move
 
     def search(self, root_state: State[TMove]) -> Node:
-        root: Node[TMove] = Node(root_state, None, None)
+        root: Node[TMove] = Node(root_state.status(), None, None)
 
         for _ in range(self.num_mcts_sims):
             node = root
@@ -87,17 +87,15 @@ class MctsSolver(Solver):
             if node.unexplored_moves:
                 move = random.choice(node.unexplored_moves)
                 state.play_move(move)
-                child_node = Node(state, parent=node, move=move)
+                child_node = Node(state.status(), parent=node, move=move)
                 node.unexplored_moves.remove(move)
                 node.children.append(child_node)
                 node = child_node
 
             # Simulation (aka rollout)
-            status = state.status()
-            while status.is_in_progress:
+            while (status := state.status()).is_in_progress:
                 move = random.choice(status.legal_moves)
                 state.play_move(move)
-                status = state.status()
 
             # Backpropagate
             while node:
