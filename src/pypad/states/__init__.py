ee6ee5790bbe55560b91218a3852b5e9dc23d2d0
Alex Cross <alexjcross90@gmail.com>
2023-05-25 23:08:59 +0100

    Expose status

--- src/pypad/states/__init__.py
+++ src/pypad/states/__init__.py
@@ -1,5 +1,6 @@
 from .connectx import ConnectXState as ConnectXState
 from .state import State as State
+from .state import Status as Status
 from .state import TMove as TMove
 from .state import TState as TState
 from .tictactoe import TicTacToeState as TicTacToeState
