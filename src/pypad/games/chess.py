bf6006facfbcdd452fd9ec44dceb88e95f97b4d4
Alex Cross <alexjcross90@gmail.com>
2023-06-10 19:25:01 +0100

    Chess action policy

--- src/pypad/games/chess.py
+++ src/pypad/games/chess.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Self
 
+import math
+
 import chess
 import numpy as np
 from chess import Board, Move, Termination
@@ -18,6 +20,7 @@ BLACK_POWERS = 2**BLACK_IDXS
 
 
 class ObsPlanes:
+    SHAPE = 19, 8, 8
     PIECES = [chess.PAWN, chess.ROOK, chess.KNIGHT, chess.BISHOP, chess.QUEEN, chess.KING]
 
     PLAYER_PAWN = 0
@@ -42,6 +45,8 @@ class ObsPlanes:
 
 
 class ActionPlanes:
+    SHAPE = 73, 8, 8
+
     QUEEN_N_1 = 0
     QUEEN_NE_1 = 1
     QUEEN_E_1 = 2
@@ -158,7 +163,85 @@ class ChessState(State[Move]):
         self.board.push(move)
 
     def policy_loc(self, move: Move) -> tuple[int, int, int]:
-        return move
+        _, rows, cols = ActionPlanes.SHAPE
+        p, r, c = self.policy_loc_3d(move)
+        idx = p * (rows * cols) + r * cols + c
+        return idx
+
+    def policy_loc_3d(self, move: Move) -> tuple[int, int, int]:
+        if self.board.turn:
+            from_idx, to_idx = move.from_square, move.to_square
+        else:
+            from_idx, to_idx = 63 - move.from_square, 63 - move.to_square
+
+        x_from, x_to = from_idx % 8, to_idx % 8
+        y_from, y_to = from_idx // 8, to_idx // 8
+        x_shift = x_to - x_from
+        y_shift = y_to - y_from
+
+        if move.promotion and move.promotion != chess.QUEEN:
+            match x_shift, y_shift, move.promotion:
+                case (-1, 1, chess.KNIGHT):
+                    return ActionPlanes.PROMOTE_KNIGHT_NW, 7 - y_from, x_from
+                case (0, 1, chess.KNIGHT):
+                    return ActionPlanes.PROMOTE_KNIGHT_N, 7 - y_from, x_from
+                case (1, 1, chess.KNIGHT):
+                    return ActionPlanes.PROMOTE_KNIGHT_NE, 7 - y_from, x_from
+                case (-1, 1, chess.ROOK):
+                    return ActionPlanes.PROMOTE_ROOK_NW, 7 - y_from, x_from
+                case (0, 1, chess.ROOK):
+                    return ActionPlanes.PROMOTE_ROOK_N, 7 - y_from, x_from
+                case (1, 1, chess.ROOK):
+                    return ActionPlanes.PROMOTE_ROOK_NE, 7 - y_from, x_from
+                case (-1, 1, chess.BISHOP):
+                    return ActionPlanes.PROMOTE_BISHOP_NW, 7 - y_from, x_from
+                case (0, 1, chess.BISHOP):
+                    return ActionPlanes.PROMOTE_BISHOP_N, 7 - y_from, x_from
+                case (1, 1, chess.BISHOP):
+                    return ActionPlanes.PROMOTE_BISHOP_NE, 7 - y_from, x_from
+                case _:
+                    raise ValueError("This should never happen")
+
+        # N, NE, E, SE, S, SW, W, NW
+        match x_shift, y_shift:
+            case (0, y):
+                if y_shift > 0:
+                    return 0 + 8 * (y_shift - 1), 7 - y_from, x_from
+                else:
+                    return 4 + 8 * (-y_shift - 1), 7 - y_from, x_from
+            case (x, 0):
+                if x_shift > 0:
+                    return 2 + 8 * (x_shift - 1), 7 - y_from, x_from
+                else:
+                    return 6 + 8 * (-x_shift - 1), 7 - y_from, x_from
+            case (x, y) if x == y:
+                if x_shift > 0:
+                    return 1 + 8 * (x_shift - 1), 7 - y_from, x_from
+                else:
+                    return 5 + 8 * (-x_shift - 1), 7 - y_from, x_from
+            case (x, y) if x == -y:
+                if x_shift > 0:
+                    return 3 + 8 * (x_shift - 1), 7 - y_from, x_from
+                else:
+                    return 7 + 8 * (-x_shift - 1), 7 - y_from, x_from
+            case (1, 2):
+                return ActionPlanes.KNIGHT_NNE, 7 - y_from, x_from
+            case (2, 1):
+                return ActionPlanes.KNIGHT_ENE, 7 - y_from, x_from
+            case (2, -1):
+                return ActionPlanes.KNIGHT_ESE, 7 - y_from, x_from
+            case (1, -2):
+                return ActionPlanes.KNIGHT_SSE, 7 - y_from, x_from
+            case (-1, -2):
+                return ActionPlanes.KNIGHT_SSW, 7 - y_from, x_from
+            case (-2, -1):
+                return ActionPlanes.KNIGHT_WSW, 7 - y_from, x_from
+            case (-2, 1):
+                return ActionPlanes.KNIGHT_WNW, 7 - y_from, x_from
+            case (-1, 2):
+                return ActionPlanes.KNIGHT_NNW, 7 - y_from, x_from
+
+        raise ValueError("This should never happen")
 
     def to_feature(self) -> np.ndarray:
         PIECE_COUNT = 6
@@ -166,6 +249,7 @@ class ChessState(State[Move]):
         CASTLING = 2
         NO_PROG = 1
         EN_PASSANT = 1
+
         PLANES = 2 * PIECE_COUNT + COL + 2 * CASTLING + NO_PROG + EN_PASSANT
 
         can_white_queen_castle = np.sign(self.board.castling_rights & chess.BB_A1)
@@ -248,7 +332,7 @@ class Chess(Game[ChessState]):
     ROWS, COLS = 8, 8
 
     def __init__(self, view: View[ChessState] | None = None) -> None:
-        self.view = view or ConsoleChessView()
+        self.view = view
 
     @property
     def name(self) -> str:
@@ -263,15 +347,12 @@ class Chess(Game[ChessState]):
         return self.ROWS, self.COLS
 
     def initial_state(self, start: str | list[int] | None = None) -> ChessState:
-        return Chess.create(start)
+        return ChessState.create(start)
 
     def config(self) -> GameParameters:
-        NUM_PLANES = 3
-
         shape = self.shape
-        observation_shape = self.ROWS, self.COLS, NUM_PLANES
-        action_size = self.ROWS * self.COLS
-        return GameParameters(shape, observation_shape, action_size)
+        action_size = math.prod(ActionPlanes.SHAPE)
+        return GameParameters(shape, ObsPlanes.SHAPE, action_size)
 
     def from_kaggle(self, obs: Observation, config: Configuration) -> ChessState:
         pass
