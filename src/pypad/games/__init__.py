99737b2068a7431648f698ef37ceb9b9b032a3de
Alex Cross <alexjcross90@gmail.com>
2023-06-15 20:40:16 +0100

    isort

--- src/pypad/games/__init__.py
+++ src/pypad/games/__init__.py
@@ -1,6 +1,6 @@
+from .game import Chess as Chess
 from .game import ConnectX as ConnectX
 from .game import Game as Game
-from .game import Chess as Chess
 from .game import TicTacToe as TicTacToe
 from .game import get_game as get_game
 from .game_type import GameType as GameType
