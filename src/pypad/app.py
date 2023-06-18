a475bc63ef73294d92d5b54ff8f39900d137b79e
Alex Cross <alexjcross90@gmail.com>
2023-06-18 21:18:34 +0100

    Add GUI app

---
+++ src/pypad/app.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from .views.chess_main import Application
+
+
+def launch() -> None:
+    loop = asyncio.get_event_loop()
+    app = Application(loop)
+    asyncio.run(app.show_async())
+
+
+if __name__ == "__main__":
+    launch()
